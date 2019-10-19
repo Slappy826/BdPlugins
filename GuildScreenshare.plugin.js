ee4d17750c32b63f5fea58cb2fe74b3eb14a09c8
Slappy826 <summon123@hotmail.com>
2019-10-19 02:39:45 -0500

    Update GuildScreenshare.plugin.js

--- GuildScreenshare.plugin.js
+++ GuildScreenshare.plugin.js
@@ -1,10 +1,10 @@
-//META{"name":"GuildScreenshare","source":"https://bd.protosmasher.net/plugins/GuildScreenshare.plugin.js"}*//
+//META{"name":"GuildScreenshare","source":"https://raw.githubusercontent.com/Slappy826/BdPlugins/master/GuildScreenshare.plugin.js"}*//
 
 class GuildScreenshare {
 
 	getName() { return "Guild Screenshare"; }
 	getDescription() { return "Allows you to screenshare in any guild."; }
-	getVersion() { return "0.0.2"; }
+	getVersion() { return "0.0.3"; }
 	getAuthor() { return "Slappy826#0001"; }
 	load() {}
 
