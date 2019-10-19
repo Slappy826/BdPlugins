aba9a57b15a0ba1e7522607c9f3b0c64f30f3e25
Slappy826 <summon123@hotmail.com>
2019-10-19 02:39:13 -0500

    Create GuildScreenshare.plugin.js

---
+++ GuildScreenshare.plugin.js
@@ -0,0 +1,27 @@
+//META{"name":"GuildScreenshare","source":"https://bd.protosmasher.net/plugins/GuildScreenshare.plugin.js"}*//
+
+class GuildScreenshare {
+
+	getName() { return "Guild Screenshare"; }
+	getDescription() { return "Allows you to screenshare in any guild."; }
+	getVersion() { return "0.0.2"; }
+	getAuthor() { return "Slappy826#0001"; }
+	load() {}
+
+	start() {
+		var mods = BdApi.findAllModules(x => x.getMetaData);
+		for(let i in mods) {
+			if(mods[i].getMetaData().title == "Guild Video"){
+				this.unpatch = BdApi.monkeyPatch(mods[i], "isAvailable", {
+					instead: e => {
+						return true;
+					}
+				});
+			}
+		}
+	}
+
+	stop() {
+        this.unpatch();
+	}
+}
