de3b9e175aaf1f1597ce17936dd0c9bcabf01028
Slappy826 <summon123@hotmail.com>
2019-11-12 18:01:11 -0600

    update plugins

---
+++ BetterEmojis.plugin.js
@@ -0,0 +1,32 @@
+//META{"name":"BetterEmojis","source":"https://raw.githubusercontent.com/Slappy826/BdPlugins/master/BetterEmojis.plugin.js"}*//
+
+class BetterEmojis {
+
+	getName() { return "Better Emojis"; }
+	getDescription() { return "improving emojis because of bad choices other people made : )" }
+	getVersion() { return "0.0.3"; }
+	getAuthor() { return "Slappy826#0001"; }
+
+	load() {}
+
+	start() {
+		this.unpatch = BdApi.monkeyPatch(BdApi.findModuleByProps("getURL"), "getURL", {
+			instead: e => {
+				if(e.methodArguments[0] == "⛓") {
+					return "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/26d3.png";
+				}
+				else if(e.methodArguments[0] == "🇲🇵") {
+					return "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f1f2-1f1f5.png"
+				}
+				else if(e.methodArguments[0] == "🔫") {
+					return "https://raw.githubusercontent.com/twitter/twemoji/42d27cd00ad8caa99577f7c9ff752a471e6ed9a5/72x72/1f52b.png";
+				}
+				return e.callOriginalMethod();
+			}
+		});
+	}
+
+	stop() {
+		this.unpatch();
+	}
+}
--- GuildScreenshare.plugin.js
+++ GuildScreenshare.plugin.js
@@ -4,7 +4,7 @@ class GuildScreenshare {
 
 	getName() { return "Guild Screenshare"; }
 	getDescription() { return "Allows you to screenshare in any guild."; }
-	getVersion() { return "0.0.4"; }
+	getVersion() { return "0.0.5"; }
 	getAuthor() { return "Slappy826#0001"; }
 	load() {
 		this.clipboard = require("electron").clipboard;
@@ -21,33 +21,9 @@ class GuildScreenshare {
 				});
 			}
 		}
-		
-		this.unpatchRender = BdApi.monkeyPatch(BdApi.findModuleByDisplayName("ChannelContextMenu").prototype, "render", {
-			after: e => {
-				try{
-					if(e.thisObject.props.type == "CHANNEL_LIST_VOICE") {
-						var menuItem = BdApi.findModuleByDisplayName("MenuItem");
-						e.returnValue.props.children.push(BdApi.React.createElement("div", {className: BdApi.findModuleByProps("itemGroup").itemGroup},
-							BdApi.React.createElement(menuItem, {
-								label: "Copy link",
-								action: () => {
-									BdApi.findModuleByProps("closeContextMenu").closeContextMenu();
-
-									var chan = e.thisObject.props.channel;
-									this.clipboard.writeText("https://discordapp.com/channels/" + chan.guild_id + "/" + chan.id);
-								}
-							})
-						));
-					}
-				}
-				catch(err) {}
-				return e.returnValue;
-			}
-		});
 	}
 
 	stop() {
         this.unpatch();
-		this.unpatchRender();
 	}
 }
