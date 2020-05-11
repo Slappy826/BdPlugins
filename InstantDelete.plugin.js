202a5f39f0af0e6f7b29e58fcfb53e57ec49a955
Slappy826 <summon123@hotmail.com>
2020-05-11 16:54:25 -0500

    update to instant delete

--- InstantDelete.plugin.js
+++ InstantDelete.plugin.js
@@ -4,11 +4,11 @@ class InstantDelete {
 
 	getName() { return "Instant Delete"; }
 	getDescription() { return "Instantly delete messages after sending them." }
-	getVersion() { return "0.0.3"; }
+	getVersion() { return "0.0.4"; }
 	getAuthor() { return "Slappy826#0001"; }
 
 	getEyeHidden() {
-		var map = BdApi.findAllModules(x => x.resolve != undefined & x.keys != undefined); 
+		var map = BdApi.findAllModules(x => x.resolve != undefined && x.keys != undefined); 
 		for(var i = 0; i < map.length; i++) { 
 			try {
 				return map[i].resolve("./EyeHidden");
@@ -21,7 +21,7 @@ class InstantDelete {
 		this.active = false;
 
 		this.pluginDir=(()=>{const e=require("process"),r=require("path");if(e.env.injDir)return r.resolve(e.env.injDir,"plugins/");switch(e.platform){case"win32":return r.resolve(e.env.appdata,"BetterDiscord/plugins/");case"darwin":return r.resolve(e.env.HOME,"Library/Preferences/","BetterDiscord/plugins/");default:return r.resolve(e.env.XDG_CONFIG_HOME?e.env.XDG_CONFIG_HOME:e.env.HOME+"/.config","BetterDiscord/plugins/")}})();let e=!1;if(!global.ZLibrary||!global.ZeresPluginLibrary||bdplugins.ZeresPluginLibrary&&(e=ZeresPluginLibrary.PluginUpdater.defaultComparator(bdplugins.ZeresPluginLibrary.plugin._config.info.version,"1.2.6"))){const r=e?"Library outdated":"Library Missing",i=BdApi.findModuleByProps("push","update","pop","popWithKey"),n=BdApi.findModuleByProps("Sizes","Weights"),t=BdApi.findModule(e=>e.defaultProps&&e.key&&"confirm-modal"==e.key()),o=()=>{require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",async(e,r,i)=>{if(e)return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");require("fs").writeFile(require("path").join(this.pluginDir,"0PluginLibrary.plugin.js"),i,()=>{setTimeout(()=>{if(!global.bdplugins.ZeresPluginLibrary)return BdApi.alert("Notice",`Due to you using EnhancedDiscord instead of BetterDiscord, you'll have to reload your Discord before ${this.getName()} starts working. Just press CTRL + R to reload and ${this.getName()} will begin to work!`);onLoaded()},1e3)})})};if(!i||!t||!n)return BdApi.alert("Uh oh",`Looks like you${e?"r Zeres Plugin Library was outdated!":" were missing Zeres Plugin Library!"} Also, failed to show a modal, so it has been ${e?"updated":"downloaded and loaded"} automatically.`),void o();i.push(i=>BdApi.React.createElement(t,Object.assign({header:r,children:[n({color:n.Colors.PRIMARY,children:[`The library plugin needed for ${this.getName()} is ${e?"outdated":"missing"}. Please click Download Now to ${e?"update":"install"} it.`]})],red:!1,confirmText:"Download Now",cancelText:"Cancel",onConfirm:()=>o()},i)))}
-		this.iconClass = webpackJsonp.push([[], {}, [[getEyeHidden()]]]).default;
+		this.iconClass = webpackJsonp.push([[], {}, [[this.getEyeHidden()]]]).default;
 	}
 
 	start() {
